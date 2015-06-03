5821ef7e6c8312a49a6af0e41bc136a01fe35cae
Robertson, Jess (Mineral Resources, Kensington) <Jesse.Robertson@csiro.au>
2015-06-03 11:33:53 +0800

    Adding measurement requests
    Also added a bunch of files to the killfile

--- bomber/__init__.py
+++ bomber/__init__.py
@@ -5,6 +5,9 @@
 """
 
 from .converters import grid_to_geotiff
+from .measurements import get_measurements
 from ._version import __version__
+from . import measurements
 
-__all__ = ['__version__', 'grid_to_geotiff']
+__all__ = ['__version__', 'grid_to_geotiff', 'get_measurements',
+		   'measurements']
